4c4b9d117437d717078f295bf9bc572148177984
Duncan Pierce <duncan@duncanpierce.org>
2021-02-20 17:35:37 +0000

    Expanded test code to explore watching nodes in the K8s cluster.

--- cmd/spike.go
+++ cmd/spike.go
@@ -4,9 +4,11 @@ import (
 	"context"
 	"fmt"
 	"github.com/spf13/cobra"
-	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	v1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
+	"time"
 )
 
 // Temporary command to explore communication with the Kubernetes API from within the cluster
@@ -27,14 +29,34 @@ func Spike(ctx context.Context) *cobra.Command {
 			if err != nil {
 				return err
 			}
-			nodeList, err := clientset.CoreV1().Nodes().List(ctx, v1.ListOptions{})
+
+			fmt.Printf("Starting watch\n")
+			nodeWatch, err := clientset.CoreV1().Nodes().Watch(ctx, metav1.ListOptions{})
 			if err != nil {
 				return err
 			}
-			for _, node := range nodeList.Items {
-				fmt.Printf("Node %s\n", node.ObjectMeta.Name)
-				for _, address := range node.Status.Addresses {
-					fmt.Printf("  %s: %s\n", address.Type, address.Address)
+			// TODO register a termination signal handler and listen to it here
+			for event := range nodeWatch.ResultChan() {
+				node, ok := event.Object.(*v1.Node)
+				if ok {
+					fmt.Printf("Node %s %s at %s\n", node.Name, event.Type, time.Now().Format("2006-01-02 15:04:05"))
+					fmt.Printf("  Phase: %s, cloud provider: %s, unschedulable: %t\n", node.Status.Phase, node.Spec.ProviderID, node.Spec.Unschedulable)
+					fmt.Printf("  Addresses:\n")
+					for _, address := range node.Status.Addresses {
+						fmt.Printf("    %s: %s\n", address.Type, address.Address)
+					}
+					fmt.Printf("  Annotations:\n")
+					for k, v := range node.Annotations {
+						fmt.Printf("    %s: %s\n", k, v)
+					}
+					fmt.Printf("  Capacity:\n")
+					for k, q := range node.Status.Capacity {
+						fmt.Printf("    %s: %s\n", k, q.String())
+					}
+					fmt.Printf("  Allocatable:\n")
+					for k, q := range node.Status.Allocatable {
+						fmt.Printf("    %s: %s\n", k, q.String())
+					}
 				}
 			}
 			return nil
