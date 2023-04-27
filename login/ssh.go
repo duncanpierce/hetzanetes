4d9737087a4f9dc16a7da195d13feae7f1e7a2da
Duncan Pierce <duncan@duncanpierce.org>
2023-04-27 19:59:57 +0100

    Run commands using SSH.

--- login/ssh.go
+++ login/ssh.go
@@ -5,8 +5,10 @@ import (
 	"crypto/rsa"
 	"crypto/x509"
 	"encoding/pem"
+	"fmt"
 	"golang.org/x/crypto/ssh"
 	"strings"
+	"time"
 )
 
 func NewSshKey() (string, string, error) {
@@ -31,4 +33,51 @@ func NewSshKey() (string, string, error) {
 	}
 
 	return publicKeyBuffer.String(), privateKeyBuffer.String(), nil
+}
+
+func Connect(hostPort string, privateKey string, timeout time.Duration, wait time.Duration, maxTries int) (*ssh.Client, *ssh.Session, error) {
+	key, err := ssh.ParsePrivateKey([]byte(privateKey))
+	if err != nil {
+		return nil, nil, fmt.Errorf("cannot parse SSH key: %w", err)
+	}
+	config := &ssh.ClientConfig{
+		Config:  ssh.Config{},
+		User:    "root",
+		Auth:    []ssh.AuthMethod{ssh.PublicKeys(key)},
+		Timeout: timeout,
+	}
+	for try := 0; try < maxTries; try++ {
+		client, err := ssh.Dial("tcp", hostPort, config)
+		if err == nil {
+			session, err := client.NewSession()
+			if err == nil {
+				return client, session, nil
+			}
+			client.Close()
+		}
+		time.Sleep(wait)
+	}
+	return nil, nil, fmt.Errorf("unable to connect to SSH server %s", hostPort)
+}
+
+func Run(hostPort string, privateKey string, timeout time.Duration, wait time.Duration, maxTries int, env map[string]string, commands []string) error {
+	client, session, err := Connect(hostPort, privateKey, timeout, wait, maxTries)
+	if err != nil {
+		return err
+	}
+	defer client.Close()
+	defer session.Close()
+	for k, v := range env {
+		err = session.Setenv(k, v)
+		if err != nil {
+			return err
+		}
+	}
+	for _, cmd := range commands {
+		err = session.Run(cmd)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
 }
