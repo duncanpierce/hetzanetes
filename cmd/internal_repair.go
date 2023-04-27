859a8ef83f40ec33a895c6e5eb0851bbcbe1b956
Duncan Pierce <duncan@duncanpierce.org>
2023-04-27 20:01:03 +0100

    Use Tick instead of NewTicker.

--- cmd/internal_repair.go
+++ cmd/internal_repair.go
@@ -19,7 +19,7 @@ func Repair() *cobra.Command {
 		Short: "Repair the cluster",
 		RunE: func(cmd *cobra.Command, args []string) error {
 			act := model.NewClusterClient()
-			for range time.NewTicker(10 * time.Second).C {
+			for range time.Tick(10 * time.Second) {
 				clusterList, err := act.GetClusterList()
 				if err != nil {
 					log.Printf("error getting clusters: %s\n", err.Error())
